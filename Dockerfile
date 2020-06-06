ad99d669e3fa732adf82b989c358c101162f2066
tphanson <tphanson@kambria.io>
2020-06-06 16:15:04 +0700

    _

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,10 @@ FROM nginx:1.18.0
 
 WORKDIR /home/thieng-proxy
 
+# RUN rm /etc/nginx/nginx.conf /etc/nginx/conf.d/default.conf
+# COPY docker-nginx/html /usr/share/nginx/html
+# COPY conf /etc/nginx
+
 RUN apt-get update
 COPY . .
 
--- packages.sh
+++ packages.sh
@@ -4,13 +4,13 @@
 set -e
 
 # Update system
-sudo apt-get update
+apt-get update
 
 # Install certbot
-sudo apt-get install software-properties-common
-sudo add-apt-repository universe
-sudo add-apt-repository ppa:certbot/certbot
-sudo apt-get update
-sudo apt-get install certbot python3-certbot-nginx
+apt-get install software-properties-common
+add-apt-repository universe
+add-apt-repository ppa:certbot/certbot
+apt-get update
+apt-get install certbot python3-certbot-nginx
 # Setup certbot to nginx
-sudo certbot --nginx
+certbot --nginx
