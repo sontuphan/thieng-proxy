b63b2ae5d5306aae9241592bf5c848213f620199
tphanson <tphanson@kambria.io>
2020-08-08 01:04:28 +0700

    debug nginx

--- nginx.sh
+++ nginx.sh
@@ -1,4 +1,4 @@
 service nginx start
-certbot --nginx -d thiengviet.com -d api.thieng.com -m phan.son.tu.1994@gmail.com --agree-tos --non-interactive
+certbot --nginx -d thiengviet.com -d api.thieng.com -m phan.son.tu.1994@gmail.com --agree-tos --non-interactive --expand
 echo "daemon off;" >> /etc/nginx/nginx.conf
 service nginx restart
