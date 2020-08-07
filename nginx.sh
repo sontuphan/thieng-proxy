fd50e63c01b40ca4e5a4834d295e582f2ac66313
tphanson <tphanson@kambria.io>
2020-08-08 01:42:53 +0700

    debug nginx

--- nginx.sh
+++ nginx.sh
@@ -3,6 +3,6 @@
 service nginx start
 chmod +x /home/thieng-proxy/challenges/authenticator.sh
 chmod +x /home/thieng-proxy/challenges/cleanup.sh
-certbot certonly --manual --preferred-challenges=http --manual-auth-hook /home/thieng-proxy/challenges/authenticator.sh --manual-cleanup-hook /home/thieng-proxy/challenges/cleanup.sh -d *.thiengviet.com -d thiengviet.com -m phan.son.tu.1994@gmail.com --agree-tos --non-interactive
+certbot certonly --manual --preferred-challenges=http --manual-auth-hook /home/thieng-proxy/challenges/authenticator.sh --manual-cleanup-hook /home/thieng-proxy/challenges/cleanup.sh -d *.thiengviet.com -d thiengviet.com -m phan.son.tu.1994@gmail.com --agree-tos --non-interactive --expand
 echo "daemon off;" >> /etc/nginx/nginx.conf
 service nginx restart
