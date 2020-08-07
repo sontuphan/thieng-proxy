4f79966f4d38dceaadbf11eedccf24ad734ec51c
tphanson <tphanson@kambria.io>
2020-08-07 22:33:04 +0700

    debug CI/CD

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM nginx:1.18.0
 
 WORKDIR /home/thieng-proxy
 
-RUN echo 'daemon off;' >> /etc/nginx/nginx.conf
 COPY configs/ /etc/nginx/conf.d/
 RUN apt-get update
 COPY . .
--- nginx.sh
+++ nginx.sh
@@ -1,2 +1,4 @@
 service nginx start
 certbot --nginx -d thiengviet.com -m phan.son.tu.1994@gmail.com --agree-tos --non-interactive
+echo "daemon off;" >> /etc/nginx/nginx.conf
+service nginx restart
